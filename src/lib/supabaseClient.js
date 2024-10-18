39567612a5fdf8945dd832062f995802a1d35e53
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2024-10-18 09:32:03 -0700

    Delete src/lib/supabaseClient.js

--- src/lib/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const PUBLIC_SUPABASE_URL = 'https://vyzeudiywhlxdzpnfehs.supabase.co';
-const PUBLIC_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5emV1ZGl5d2hseGR6cG5mZWhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYwOTI0NjQsImV4cCI6MjA0MTY2ODQ2NH0.a4mNwSC0P1XlA4Cz5uHUEWPLeQWY7t7B66mZ4W-HSH8';
-
-export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
